e1c4cf1d848a4240481669171a9ced18a7723b02
shivaleela <ammukatii@gmail.com>
2025-03-06 15:42:00 +0530

    something added

--- src/main/com/cartpage.java
+++ src/main/com/cartpage.java
@@ -3,7 +3,19 @@ import org.testng.annotations.Test;
 public class cartpage {
 
 	@Test()
+	public void login()
+	{
+		System.out.println("hello");
+		System.out.println("hello");
+	}
+	@Test()
 	public void logOut()
+	{
+		System.out.println("hello");
+		System.out.println("hello");
+	}
+	@Test()
+	public void logOut1()
 	{
 		System.out.println("hello");
 		System.out.println("hello");
